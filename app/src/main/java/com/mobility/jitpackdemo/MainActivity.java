a14ca6dbb9fbbbba5a468ff7296a10c7c2088542
cariq-02 <sneha.ranjitkumar@mycariq.com>
2024-02-16 16:35:02 +0530

    initial commit

---
+++ app/src/main/java/com/mobility/jitpackdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mobility.jitpackdemo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mobility.utility_lib.CarIQUtil;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        CarIQUtil.
+    }
+}
---
+++ utility-lib/src/main/java/com/mobility/utility_lib/CarIQUtil.java
@@ -0,0 +1,11 @@
+package com.mobility.utility_lib;
+
+import android.util.Log;
+
+public class CarIQUtil {
+
+    public static void printLog(String tag, String message) {
+        Log.d(tag, "printLog: " + message);
+    }
+}
+
